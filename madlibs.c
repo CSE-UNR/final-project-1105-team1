c0eed4fa8b9a02447043c6666b751fd44387f1b2
Luke Wright <luke_wright05@hotmail.com>
2024-12-08 22:55:25 +0000

    first version

--- madlibs.c
+++ madlibs.c
@@ -7,28 +7,47 @@
 #define ROW_CAP 20
 #define COL_CAP 100
 
-void madlibRead(int col, int row, char str[][col]);
+void madlibRead(FILE* fin, int col, int row, char str[][col]);
 void madlibDisplay(int col, int row, char str[][col]);
 void madlibA(int col, int row, int user, char str[][col]);
 void madlibN(int col, int row, int user, char str[][col]);
 void madlibV(int col, int row, int user, char str[][col]);
+void strCopy(char source[][COL_CAP], char dest[][COL_CAP]);
 
 int main(){
 
 	char madLib[ROW_CAP][COL_CAP];
-	int c;
 	FILE* fp;
 	
 	fp = fopen(FILE_NAME, "r");
 	
-	c = strLength(madLib);
+	madlibRead(fp, COL_CAP, ROW_CAP, madLib);
 	
-	printf("%d", c);
-
 	return 0;
 	
 }
 
-void madlibRead(int col, int row, char str[][col]){
+void madlibRead(FILE* fin, int col, int row, char str[][col]){
 
+	for(row = 0; str[row][col] != '\0'; row++){
+		for(col = 0; str[row][col] != '\n'; col++){
+		
+			fscanf(fin, "%c", &str[row][col]);
+			
+		}
+	}
+}
+
+void strCopy(char source[][COL_CAP], char dest[][COL_CAP]){
+
+	for(row = 0; source[row][col] != '\0'; row++){
+		for(col = 0; source[row][col] != '\n'; col++){
+		
+			dest[row][col] = source[row][col];
+			
+		}
+	}
 	
+	dest[row][col] = '\0';
+	
+}
