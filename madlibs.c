9a5653c545b4f7a21bf589dfcf5931ee75776eda
Luke Wright <luke_wright05@hotmail.com>
2024-12-08 22:31:40 +0000

    first version

--- madlibs.c
+++ madlibs.c
@@ -4,6 +4,31 @@
 #include <stdio.h>
 
 #define FILE_NAME "madlib2.txt"
-#define WORD_CAP 20
+#define ROW_CAP 20
+#define COL_CAP 100
 
-void strLength(char str[]
+void madlibRead(int col, int row, char str[][col]);
+void madlibDisplay(int col, int row, char str[][col]);
+void madlibA(int col, int row, int user, char str[][col]);
+void madlibN(int col, int row, int user, char str[][col]);
+void madlibV(int col, int row, int user, char str[][col]);
+
+int main(){
+
+	char madLib[ROW_CAP][COL_CAP];
+	int c;
+	FILE* fp;
+	
+	fp = fopen(FILE_NAME, "r");
+	
+	c = strLength(madLib);
+	
+	printf("%d", c);
+
+	return 0;
+	
+}
+
+void madlibRead(int col, int row, char str[][col]){
+
+	
