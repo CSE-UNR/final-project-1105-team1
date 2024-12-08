63f0f79c2de39bea97ce84315b979efc090caff8
Luke Wright <luke_wright05@hotmail.com>
2024-12-08 22:04:38 +0000

    first version

---
+++ madlibs.c
@@ -0,0 +1,9 @@
+//Authors: Luke Wright, Aiden Libanag, & Jordan Stephenson
+//Date 12/8/2024
+
+#include <stdio.h>
+
+#define FILE_NAME "madlib2.txt"
+#define WORD_CAP 20
+
+void strLength(char str[]
