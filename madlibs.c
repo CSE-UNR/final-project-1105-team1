76369413fba1773b28e063da7e834d4cfb944ab0
Luke Wright <luke_wright05@hotmail.com>
2024-12-09 21:53:04 +0000

    next version

--- madlibs.c
+++ madlibs.c
@@ -7,21 +7,41 @@
 #define ROW_CAP 17
 #define COL_CAP 17
 
-void madlibRead(FILE* fin, char str[][COL_CAP]);
+void madlibRead(FILE* fin, char str[][COL_CAP], char ind[]);
 void madlibDisplay(char str[][COL_CAP]);
-void madlibA(int user, char str[][COL_CAP]);
-void madlibN(int user, char str[][COL_CAP]);
-void madlibV(int user, char str[][COL_CAP]);
-void strCopy(char source[][COL_CAP], char dest[][COL_CAP]);
+int madlibA(char user[], char str[][COL_CAP]);
+void strCopy(char source[], char dest[]);
 
 int main(){
 
-	char madLib[ROW_CAP][COL_CAP];
+	char madLib[ROW_CAP][COL_CAP], User[COL_CAP], prompt[COL_CAP];
 	FILE* fp;
 	
 	fp = fopen(FILE_NAME, "r");
 	
-	madlibRead(fp, madLib);
+	madlibRead(fp, madLib, prompt);
+	
+	for(int index = 0; index < 7; index++){
+	
+		switch(prompt[index]){
+		
+			case 'A':
+				printf("Please enter an adjective: ");
+				scanf("%s", User);
+			break;
+			case 'V':
+				printf("Please enter a verb: ");
+				scanf("%s", User);
+			break;
+			case 'N':
+				printf("Please enter a noun: ");
+				scanf("%s", User);
+			break;
+			madlibA(User, madLib);
+		}
+		
+		
+	}
 	
 	madlibDisplay(madLib);
 	
@@ -31,30 +51,39 @@ int main(){
 	
 }
 
-void madlibRead(FILE* fin, char str[][COL_CAP]){
+void madlibRead(FILE* fin, char str[][COL_CAP], char ind[]){
 
 	for(int row = 0; row < ROW_CAP; row++){
 		for(int col = 0; col < COL_CAP; col++){
 		
 			fscanf(fin, "%c", &str[row][col]);
 			
+			if(str[row][col] == '\n'){
+	
+				str[row][col] = ' ';
+			}
+			
+			if(str[row][col] == 'A' || str[row][col] == 'N' || str[row][col] == 'V'){
+			
+				ind[row] = str[row][col];
+			
+			}
 		}
 	}
 }
 
-void strCopy(char source[][COL_CAP], char dest[][COL_CAP]){
+void strCopy(char source[], char dest[]){
 
-	int row = 0, col = 0;
+	int index;
 
-	for(row = 0; source[row][col] != '\0'; row++){
-		for(col = 0; source[row][col] != '\n'; col++){
+	for(index = 0; source[index] != '\0'; index++){
+	
+		dest[index + 1] = source[index];
 		
-			dest[row][col] = source[row][col];
-			
-		}
 	}
 	
-	dest[row][col] = '\0';
+	//dest[index + 1] = ' ';
+	//dest[1] = ' ';
 	
 }
 
@@ -66,3 +95,19 @@ void madlibDisplay(char str[][COL_CAP]){
 		}
 	}
 }	
+
+int madlibA(char user[], char str[][COL_CAP]){
+
+	int count = 0;
+
+	for(int row = 0; row < ROW_CAP; row++){
+		for(int col = 0; col < COL_CAP; col++){
+			if(str[row][col] == 'A' || str[row][col] == 'N' || str[row][col] == 'V'){
+				strCopy(user, str[row]);
+				
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
